543a433744a40cdbd54055e2bc4a3676e43fd7d2
Maklolaj <m.zelek@op.pl>
2021-10-01 20:17:59 +0200

    eslintignore

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,21 @@
 module.exports = {
-  'env': {
-    'browser': true,
-    'es2021': true,
+  env: {
+    browser: true,
+    es2021: true,
   },
-  'extends': [
-    'google',
-    'plugin:prettier/recommended',
-  ],
-  'parser': '@typescript-eslint/parser',
-  'parserOptions': {
-    'ecmaVersion': 12,
-    'ecmaFeatures': {
-      'jsx': true,
-      'modules': true
-    }
+  extends: ["google", "plugin:prettier/recommended"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 12,
+    ecmaFeatures: {
+      jsx: true,
+      modules: true,
+    },
   },
-  'plugins': [
-    '@typescript-eslint',
-    'prettier',
-  ],
-  'rules': {
+  plugins: ["@typescript-eslint", "prettier"],
+  rules: {
     "require-jsdoc": "off",
     "new-cap": "off",
-    "prettier/prettier": "error"
+    "prettier/prettier": "error",
   },
 };
