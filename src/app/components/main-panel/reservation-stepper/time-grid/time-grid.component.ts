4734f70d14ac06f5290df65063999e6140f3462a
Maklolaj <m.zelek@op.pl>
2021-11-20 19:40:06 +0100

    res

--- src/app/components/main-panel/reservation-stepper/time-grid/time-grid.component.ts
+++ src/app/components/main-panel/reservation-stepper/time-grid/time-grid.component.ts
@@ -3,13 +3,25 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-time-grid',
   templateUrl: './time-grid.component.html',
-  styleUrls: ['./time-grid.component.scss']
+  styleUrls: ['./time-grid.component.scss'],
 })
 export class TimeGridComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
-  }
+    let persons: { [id: string]: IReservation } = {};
 
+    persons['p1'] = { firstName: 'F1', lastName: 'L1' };
+    persons['p2'] = { firstName: 'F2', lastName: 'P3' }; // will result in an error
+
+    for (let key in persons) {
+      console.log(persons[key]['lastName']);
+      // Use `key` and `value`
+    }
+  }
+}
+
+export interface IReservation {
+  firstName: string;
+  lastName: string;
 }
