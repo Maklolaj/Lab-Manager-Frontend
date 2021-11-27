6370581d3fed619444e03885256f99427a11e43d
Maklolaj <m.zelek@op.pl>
2021-11-27 19:35:04 +0100

    get data

--- src/app/components/main-panel/reservation-stepper/time-grid/time-grid.component.ts
+++ src/app/components/main-panel/reservation-stepper/time-grid/time-grid.component.ts
@@ -14,19 +14,7 @@ export class TimeGridComponent implements OnInit {
   reservationList: IReservationFromDateModel[] = [];
 
   ngOnInit(): void {
-    let persons: { [id: string]: IReservation } = {};
-
-    persons['p1'] = { firstName: 'F1', lastName: 'L1' };
-    persons['p2'] = { firstName: 'F2', lastName: 'P3' }; // will result in an error
-
-    for (let key in persons) {
-      console.log(persons[key]['lastName']);
-      // Use `key` and `value`
-    }
+    console.log(this.reservationList)
   }
 }
 
-export interface IReservation {
-  firstName: string;
-  lastName: string;
-}
