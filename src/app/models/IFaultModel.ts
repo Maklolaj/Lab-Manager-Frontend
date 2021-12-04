c581e27be958016457392e286c5b23df432202ff
Maklolaj <m.zelek@op.pl>
2021-12-04 12:57:06 +0100

    fault model

---
+++ src/app/models/IFaultModel.ts
@@ -0,0 +1,10 @@
+export interface IItemModel {
+    id: number;
+    item: number;
+    user: string;
+    manufacturer: string;
+    description: string;
+    reportTIme: Date;
+    isDeleted: boolean;
+  }
+
---
+++ src/app/services/fault.service.ts
@@ -0,0 +1,11 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class FaultService {
+  constructor(private httpClient: HttpClient) {}
+}
