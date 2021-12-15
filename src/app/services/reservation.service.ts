cb271cd972e3887301119bcb71be41d6b0dab357
Maklolaj <m.zelek@op.pl>
2021-12-15 21:37:19 +0100

    remove redundant

--- src/app/services/reservation.service.ts
+++ src/app/services/reservation.service.ts
@@ -2,7 +2,6 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment.prod';
-import { IItemModel } from '../models/IItemModel';
 import { IReservationFromDateModel } from '../models/ReservationModels/IReservationFromDateModel';
 import { IReservationModel } from '../models/ReservationModels/IReservationModel';
 
