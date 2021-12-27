aac29476e3871c5442a75e21ffacfccf15ebac0e
Maklolaj <m.zelek@op.pl>
2021-12-27 10:27:12 +0100

    registration validation

--- src/app/components/auth/register/register.component.ts
+++ src/app/components/auth/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -10,12 +11,23 @@ import { Router } from '@angular/router';
 export class RegisterComponent implements OnInit {
   registerForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder, private router: Router) {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private authService: AuthService
+  ) {}
 
   ngOnInit() {
     this.registerForm = this.formBuilder.group({
-      email: ['', Validators.required],
-      login: ['', Validators.required],
+      email: [
+        '',
+        [
+          Validators.required,
+          Validators.email,
+          Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$'),
+        ],
+      ],
+      login: ['', Validators.required, Validators.minLength(6)],
       passwordOne: ['', [Validators.required, Validators.minLength(6)]],
       passwordTwo: ['', [Validators.required, Validators.minLength(6)]],
     });
@@ -26,6 +38,26 @@ export class RegisterComponent implements OnInit {
   }
 
   onSubmit(): void {
-    console.log(this.f);
+    this.validateForm() ? alert('registered successfully') : null; //this.authService.registerUser(this.f.email.value,this.f.passwordOne.value)
+  }
+
+  validateForm(): boolean {
+    if (!this.f.email.valid) {
+      alert('Nieprawidłowy E-mail');
+      return false;
+    }
+    if (!this.f.passwordOne.valid || !this.f.passwordTwo.valid) {
+      alert('Hasła muszą mieć minimum 6 znaków');
+      return false;
+    }
+    if (this.f.passwordOne.value !== this.f.passwordTwo.value) {
+      alert('Hasła muszą być identyczne');
+      return false;
+    }
+    if (!this.registerForm.valid) {
+      alert('Nieprawidłowe dane w formularzu');
+      return false;
+    }
+    return true;
   }
 }
