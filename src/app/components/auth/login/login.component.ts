518605234ab3e74cadbc438d966f03db53f334bf
Maklolaj <m.zelek@op.pl>
2022-01-09 15:00:23 +0100

    login impr

--- src/app/components/auth/login/login.component.ts
+++ src/app/components/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { AuthService } from 'src/app/services/auth.service';
@@ -15,26 +16,35 @@ export class LoginComponent implements OnInit {
     private router: Router,
     private authService: AuthService
   ) {}
-  tempEmail: string = 'test@test.pl';
-  tempPass: string = 'Test1234!';
+  userEmail = new FormControl('');
+  userPass = new FormControl('');
 
   ngOnInit(): void {}
 
   signIn(): void {
     this.store.dispatch(siginIn({ isAdmin: false }));
 
-    this.authService.signIn(this.tempEmail, this.tempPass).subscribe((jwt) => {
-      localStorage.setItem('jwt', JSON.stringify(jwt));
+    this.authService
+      .signIn(this.userEmail.value, this.userPass.value)
+      .subscribe(
+        (jwt) => {
+          localStorage.setItem('jwt', JSON.stringify(jwt));
 
-      let decodedJWT = atob(
-        JSON.parse(localStorage.getItem('jwt')!)['token'].split('.')[1]
-      );
+          let decodedJWT = atob(
+            JSON.parse(localStorage.getItem('jwt')!)['token'].split('.')[1]
+          );
 
-      this.store.dispatch(
-        siginIn({ isAdmin: JSON.parse(decodedJWT)['prn'] === '606' })
+          this.store.dispatch(
+            siginIn({ isAdmin: JSON.parse(decodedJWT)['prn'] === '606' })
+          );
+          console.log(jwt);
+          console.log(decodedJWT);
+          this.router.navigate(['app-main-panel']);
+        },
+        (error) => {
+          console.log(error);
+        }
       );
-    });
-    this.router.navigate(['app-main-panel']);
   }
 
   register(): void {
