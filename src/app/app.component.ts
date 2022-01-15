cc2d7a5976816a62f8867915a5429327cd4e8b4b
Maklolaj <m.zelek@op.pl>
2022-01-15 22:47:29 +0100

    cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,8 @@
-import { Router } from '@angular/router';
-import { Component, Inject } from '@angular/core';
-import { Store } from '@ngrx/store';
-import { Observable, Subscription } from 'rxjs';
-import { slectLoginStatus } from './store/selectors/auth.selectors';
-import { IMainPageState } from './store/models/IAuthState';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  isUserLogged: IMainPageState = {
-    isSigningIn: false,
-    isAdminSigningIn: false,
-  };
-
-  constructor(private store: Store<any>) {
-    this.store.select(slectLoginStatus).subscribe((x: any) => {
-      this.isUserLogged.isSigningIn = x['isSigningIn'];
-      this.isUserLogged.isAdminSigningIn = x['isAdminSigningIn'];
-    });
-  }
-}
+export class AppComponent {}
