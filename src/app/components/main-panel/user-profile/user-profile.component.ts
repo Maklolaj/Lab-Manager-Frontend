98c22647c62a30b26895720341692cea0eaa973b
Maklolaj <m.zelek@op.pl>
2022-01-29 21:37:52 +0100

    user profile form

--- src/app/components/main-panel/user-profile/user-profile.component.ts
+++ src/app/components/main-panel/user-profile/user-profile.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-user-profile',
@@ -6,17 +8,56 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-profile.component.scss'],
 })
 export class UserProfileComponent implements OnInit {
-  constructor() {}
+  constructor(
+    private authService: AuthService,
+    private formBuilder: FormBuilder
+  ) {}
 
-  ngOnInit(): void {}
+  updateUserDataForm: FormGroup;
 
-  email: string = 'test@test.pl';
+  ngOnInit(): void {
+    this.updateUserDataForm = this.formBuilder.group({
+      email: [
+        '',
+        [
+          Validators.required,
+          Validators.email,
+          Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$'),
+        ],
+      ],
+      passwordOne: ['', [Validators.required, Validators.minLength(6)]],
+      passwordTwo: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
 
-  passwordOne: string = '';
-
-  passwordTwo: string = '';
+  get fromControls() {
+    return this.updateUserDataForm.controls;
+  }
 
   changeUserData() {
-    alert(`Dane zostały zmienione`);
+    this.authService
+      .updateUserData({
+        userEmail: this.fromControls.email.value,
+        userPassword: this.fromControls.passwordOne.value,
+        userReTypedPassword: this.fromControls.passwordTwo.value,
+      })
+      .subscribe(
+        (x) => {
+          console.log(x);
+          this.clearOutForm();
+          alert('Operacja udana');
+        },
+        (error) => {
+          console.log(error);
+          this.clearOutForm();
+          alert('Operacja nieudana');
+        }
+      );
+  }
+
+  clearOutForm(): void {
+    this.fromControls.email.setValue('');
+    this.fromControls.passwordOne.setValue('');
+    this.fromControls.passwordTwo.setValue('');
   }
 }
